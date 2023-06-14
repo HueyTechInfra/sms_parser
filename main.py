bc14b3228af0c147d4b0d1ad839d4e4de43390b0
SanchayKe <sanchay.kedia.int@paymeindia.in>
2023-06-14 11:59:29 +0530

    added comments for better readability

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import parser_1 as parser
 
+# patters for credit messages:
 regex = r"(?i).*(?:credited|received|Credited|Received)\D*(INR|Rs.)\D*(\d+(?:\.\d+)?)"
 regex1 = r"(?i).*(?:credited|received|Credited|Received)\D*(INR|Rs.)\D*(\d+(?:\.\d+)?)"
 regex2 = r"(?i)(Rs.|INR)\D*(\d+(?:\.\d+)?).*(?:credited|received|Credited|Received)"
@@ -16,9 +17,12 @@ regex3 = r"(?i).*(Rs.|INR)\.*(\d+(?:\.\d+)?).*(?:credited|received|Credited|Rece
 regex4 = r"(?:Rs\.?\s*|INR)\s*(\d+(?:\.\d+)?)\s*(debited|withdrawn)"
 regex5 = r"(?i)debited \D*(?:Rs\.|INR)?\s*(\d+(?:\.\d+)?)"
 
+# pattern for ambiguous sms
 pattern_w = r"(?i).*(received|request|requested).*(received|request|requested)"
 pattern_w1 = r"(?i).*(debited|debit)"
-format = "%Y-%m-%d"
+
+format = "%Y-%m-%d"  # used for extraction human-readable date from json date format
+
 with open('data.json', 'r', encoding='utf-8') as json_file:
     data = json.load(json_file)
     s_time1 = re.sub("\D", '', "/Date(" + str(data[0]["date"]) + ")/")
@@ -30,15 +34,17 @@ with open('data.json', 'r', encoding='utf-8') as json_file:
         s_time1 = re.sub("\D", '', "/Date(" + str(data[x]["date"]) + ")/")
         d_time1 = datetime.fromtimestamp(float(s_time1) / 1000).strftime('%Y-%m-%d')
         d_time = datetime.strptime(d_time1, format)
+
+        # calculation the range of dataset
         if date_min > d_time: date_min = d_time
         if date_max < d_time: date_max = d_time
+
     duration = (date_max - date_min).days
+
     print("we have sms data of total", (date_max - date_min).days, "that is from", date_min, "to", date_max)
-    count_overdue = 0
-    count_emi = 0
+    count_overdue = count_emi = 0
     sum = 0
-    acc_bal_amt = 0
-    acc_count = 0
+    acc_bal_amt = acc_count = 0
     sum_debit_30 = sum_debit_60 = sum_debit_90 = sum_debit = 0
     count_debit_30 = count_debit_60 = count_debit_90 = count_debit = 0
     sum_credit_30 = sum_credit_60 = sum_credit_90 = sum_credit = 0
@@ -70,22 +76,26 @@ with open('data.json', 'r', encoding='utf-8') as json_file:
     sheet = workbook.active
 
     for x in range(len(data)):
-        val = 0
+        val = 0  # pointer to decide later if sms if of financial type or promotional type
+
         s_time1 = re.sub("\D", '', "/Date(" + str(data[x]["date"]) + ")/")
         d1 = datetime.fromtimestamp(float(s_time1) / 1000).strftime('%Y-%m-%d')
         d = datetime.strptime(d1, format)
         decline_bit = 0
-        msg = data[x]["body"]
+        msg = data[x]["body"]  # msg variable stores the body of the sms
+
+        # check if msg contains the word EMI.
         if re.search(r".*EMI.*", msg, re.IGNORECASE):
-            # msg contains the word EMI.
+
             # check if it is a reminder message
             if re.search(r"\b(?:pending|overdue|due|earliest|clear|presented)\b", msg, re.IGNORECASE):
-                count_overdue+=1
+                count_overdue += 1
                 str_temp.append(msg)
                 val = 1
                 category.append("reminder")
                 amountdebited.append(None)
                 amountcredited.append(None)
+
                 # if the msg contains amount to be paid
                 if re.search(r"EMI.*?Rs\.?\s*([\d,.]+).*?(due|earliest)", msg):
                     match = re.search(r"EMI.*?Rs\.?\s*([\d,.]+).*?(due|earliest)", msg)
@@ -94,7 +104,7 @@ with open('data.json', 'r', encoding='utf-8') as json_file:
                     amount.append(None)
             else:
                 # emi debit msg
-                count_emi+=1
+                count_emi += 1
                 match1 = re.search(regex4, msg, re.IGNORECASE)
                 match2 = re.search(regex5, msg, re.IGNORECASE)
 
@@ -111,11 +121,17 @@ with open('data.json', 'r', encoding='utf-8') as json_file:
                     category.append("EMI")
         elif re.match(pattern_w, msg):
             pass
-        elif re.match(r'.*(legal notice)',msg,re.IGNORECASE):
-            cnt_legal+=1
-            if(date_max-d).days <= 30: cnt_legal_30+=1
-            if (date_max - d).days <= 60: cnt_legal_60 += 1
-            if (date_max - d).days <= 90: cnt_legal_90 += 1
+        elif re.match(r'.*(legal notice)', msg, re.IGNORECASE):
+            cnt_legal += 1
+
+            # counting number of legal notices in different duration slabs
+            if (date_max - d).days <= 30:
+                cnt_legal_30 += 1
+            if (date_max - d).days <= 60:
+                cnt_legal_60 += 1
+            if (date_max - d).days <= 90:
+                cnt_legal_90 += 1
+
         # finding occurrences of declined payments
         elif re.search(r'\b(declined|decline)[^a-zA-Z](.*?)[\.]', msg):
             val = 1
@@ -146,6 +162,8 @@ with open('data.json', 'r', encoding='utf-8') as json_file:
             amount.append(None)
             sum_credit += float(match.group(2))  # lifetime credit
             count_credit += 1
+
+            # calculating sum of amt credited and number of credit transactions in different duration slabs
             if (date_max - d).days <= 30:
                 sum_credit_30 += float(match.group(2))
                 count_credit_30 += 1
@@ -166,6 +184,8 @@ with open('data.json', 'r', encoding='utf-8') as json_file:
             amount.append(None)
             sum_credit += float(match.group(2))  # lifetime credit
             count_credit += 1
+
+            # calculating sum of amt credited and number of credit transactions in different duration slabs
             if (date_max - d).days <= 30:
                 sum_credit_30 += float(match.group(2))
                 count_credit_30 += 1
@@ -189,6 +209,8 @@ with open('data.json', 'r', encoding='utf-8') as json_file:
             amount.append(None)
             sum_credit += float(match.group(2))  # lifetime credit
             count_credit += 1
+
+            # calculating sum of amt credited and number of credit transactions in different duration slabs
             if (date_max - d).days <= 30:
                 sum_credit_30 += float(match.group(2))
                 count_credit_30 += 1
@@ -200,7 +222,7 @@ with open('data.json', 'r', encoding='utf-8') as json_file:
                 count_credit_90 += 1
             val = 1
 
-        # bounce transactions are fetched here
+        # categorizing bounce transactions
         elif re.search(r"\b(bounce)\b", msg, re.IGNORECASE):
             match = re.search(r"\b(bounce)\b", msg, re.IGNORECASE)
             val = 1
@@ -221,13 +243,15 @@ with open('data.json', 'r', encoding='utf-8') as json_file:
             if (date_max - d).days <= 90:
                 count_bounce_90 += 1
 
-        # finding debit messages and finding amount debited
+        # finding debit messages and amount debited
         elif re.search(regex4, msg):
             val = 1
             match = re.search(regex4, msg, re.IGNORECASE)
             amountdebited.append(float(match.group(1)))
             sum_debit += float(match.group(1))  # lifetime debit
             count_debit += 1
+
+            # calculating sum of amt debited and number of debit transactions in different duration slabs
             if (date_max - d).days <= 30:
                 sum_debit_30 += float(match.group(1))
                 count_debit_30 += 1
@@ -248,6 +272,8 @@ with open('data.json', 'r', encoding='utf-8') as json_file:
             amountdebited.append(float(match.group(1)))
             sum_debit += float(match.group(1))  # lifetime debit
             count_debit += 1
+
+            # calculating sum of amt debited and number of debit transactions in different duration slabs
             if (date_max - d).days <= 30:
                 sum_debit_30 += float(match.group(1))
                 count_debit_30 += 1
@@ -275,23 +301,29 @@ with open('data.json', 'r', encoding='utf-8') as json_file:
             pattern5 = r"NEFT.*?(\d+)"  # to extarct NEFT
             pattern6 = r"UPI/(?:CRADJ|P2A)/([^/]+)"  # to extract UPI
             pattern7 = r"Info\D*(\d{6,})"  # to extract Info
-            pattern8 = r"(?i)(?:Rs\.?|INR)\s*([\d,]+\.\d+|\d+(?:,\d+)?)\b.*?\bdebited\b.*?\bcard\b" # regex for card transactions
-            #regex for atm transactions
+
+            # regex for card transactions
+            pattern8 = r"(?i)(?:Rs\.?|INR)\s*([\d,]+\.\d+|\d+(?:,\d+)?)\b.*?\bdebited\b.*?\bcard\b"
+            # regex for atm transactions
             pattern9 = r"(?i)(?:Rs\.?|INR)\s*(?:\S+\s+)?([\d,.]+).*?withdrawn.*?(?:\s+\S+)?\s+atm"
             pattern10 = r"(?i)Debited\s*(?:\S+\s+)?(?:Rs\.?|INR)\s*(?:\S+\s+)?([\d,.]+).*?CASH-ATM"
-            match1 = re.search(pattern, msg)
-            match2 = re.search(pattern1, msg)
-            match3 = re.search(pattern2, msg)
-            match4 = re.search(pattern3, msg)
-            match5 = re.search(pattern4, msg)
+
+            match1 = re.search(pattern, msg, re.IGNORECASE)
+            match2 = re.search(pattern1, msg, re.IGNORECASE)
+            match3 = re.search(pattern2, msg, re.IGNORECASE)
+            match4 = re.search(pattern3, msg, re.IGNORECASE)
+            match5 = re.search(pattern4, msg, re.IGNORECASE)
             match6 = re.search(pattern5, msg, re.IGNORECASE)
-            match7 = re.search(pattern6, msg)
-            match8 = re.search(pattern7, msg)
+            match7 = re.search(pattern6, msg, re.IGNORECASE)
+            match8 = re.search(pattern7, msg, re.IGNORECASE)
             match9 = re.search(pattern8, msg, re.IGNORECASE)
             match10 = re.search(pattern9, msg, re.IGNORECASE)
             match11 = re.search(pattern10, msg, re.IGNORECASE)
-            if decline_bit == 0: reason.append(None)
-            date_sms.append(d)
+            if decline_bit == 0:  # pointer to see if the sms is of type payment declined
+                reason.append(None)
+
+            date_sms.append(d)  # appending date for each sms to show in output Excel file
+
             # finding account number if present in the sms
             if match1:
                 account_no.append(int(match1.group(2)[-4:]))
@@ -385,8 +417,10 @@ with open('data.json', 'r', encoding='utf-8') as json_file:
                     sum_card_90 += amount_card
             # finding atm transactions sum and count in different time slabs
             elif match10 or match11:
-                if match10: match = match10
-                else: match = match11
+                if match10:
+                    match = match10
+                else:
+                    match = match11
                 amount_atm = float(match.group(1).replace(',', ''))
                 count_atm += 1
                 sum_atm += amount_atm
@@ -400,20 +434,22 @@ with open('data.json', 'r', encoding='utf-8') as json_file:
                     count_atm_90 += 1
                     sum_atm_90 += amount_atm
 
-        if bool(re.search(r'\b(mab)\b', msg, re.IGNORECASE)):  # finding mab occurrences
+        if bool(re.search(r'\b(mab)\b', msg, re.IGNORECASE)):  # finding mab occurrences in different duration slabs
             cnt_below_mab_penalty_occurances += 1
             if (date_max - d).days <= 30: cnt_below_mab_penalty_occurances_30 += 1
             if (date_max - d).days <= 60: cnt_below_mab_penalty_occurances_60 += 1
             if (date_max - d).days <= 90: cnt_below_mab_penalty_occurances_90 += 1
+
     avg_acc_bal = int(acc_bal_amt / acc_count)  # avg account balance
     # Create a new DataFrame with messages and amounts debited
     new_df = pd.DataFrame({'Message': str_temp, 'Amount Debited': amountdebited, 'Amount Credited': amountcredited,
                            'Account Number': account_no, 'Transaction ID': transaction_id, "Amount": amount,
                            "Category": category, "reason": reason, "Account Balance": acc_bal, "Date": date_sms})
+
     # Save the new DataFrame to a new Excel file
     new_df.to_excel('messages_with_amountsV2.xlsx', index=False)
 
-    # computing values for debit transactions
+    # computing parameters for debit transactions
     cnt_avg_daily_debit_trans = count_debit / duration
     cnt_avg_daily_debit_trans_90 = count_debit_90 / 90
     cnt_avg_daily_debit_trans_60 = count_debit_60 / 60
@@ -433,11 +469,13 @@ with open('data.json', 'r', encoding='utf-8') as json_file:
     ratio_amt_avg_debit_per_trans_30_to_90 = amt_avg_debit_per_trans_30 / amt_avg_daily_debit_trans_90
     ratio_amt_avg_debit_per_trans_30_to_60 = amt_avg_debit_per_trans_30 / amt_avg_daily_debit_trans_60
 
-    print("Count of avg daily debit transactions",cnt_avg_daily_debit_trans)
-    print("Count of avg daily debit transactions for last 90 days is",cnt_avg_daily_debit_trans_90)
-    print("amount of average debit transaction is",amt_avg_debit_per_trans)
-    print("ratio of average amount debited daily within last 30 days to last 90 days is ", ratio_amt_avg_daily_debit_trans_30_to_90)
-    print("ratio of average amount debited daily per transaction within last 30 days to last 90 days is ", ratio_amt_avg_debit_per_trans_30_to_90)
+    print("Count of avg daily debit transactions", cnt_avg_daily_debit_trans)
+    print("Count of avg daily debit transactions for last 90 days is", cnt_avg_daily_debit_trans_90)
+    print("amount of average debit transaction is", amt_avg_debit_per_trans)
+    print("ratio of average amount debited daily within last 30 days to last 90 days is ",
+          ratio_amt_avg_daily_debit_trans_30_to_90)
+    print("ratio of average amount debited daily per transaction within last 30 days to last 90 days is ",
+          ratio_amt_avg_debit_per_trans_30_to_90)
 
     # computing parameters for credit transactions
     cnt_avg_daily_credit_trans = count_credit / duration
@@ -470,10 +508,10 @@ with open('data.json', 'r', encoding='utf-8') as json_file:
     print(amt_avg_credit_per_trans)
     print(ratio_cnt_avg_daily_credit_trans_30_to_90)
     print(ratio_amt_avg_credit_per_trans_30_to_90)
-    print("customer has had total",cnt_below_mab_penalty_occurances,"below mab occurences")
-    print("total number of neft/imps/rtgs payments have been",cnt_neft_rtgs_imps_tran)
-    print("customer has average account balance",avg_acc_bal)
-    print("number of payments declined have been",count_decline)
+    print("customer has had total", cnt_below_mab_penalty_occurances, "below mab occurences")
+    print("total number of neft/imps/rtgs payments have been", cnt_neft_rtgs_imps_tran)
+    print("customer has average account balance", avg_acc_bal)
+    print("number of payments declined have been", count_decline)
 
     # plotting graphs
     categories = ['legal notices', 'below mab occurrences', 'debited successfully', 'declined payments', 'bounced '
@@ -532,6 +570,5 @@ with open('data.json', 'r', encoding='utf-8') as json_file:
 
 json_file.close()
 workbook.save('complete_data.xlsx')
-
 
 parser.plot()
